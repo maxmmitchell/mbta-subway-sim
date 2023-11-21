0ff062c8988279d2928f5e9d9d91c1e55478f8a9
Max Mitchell <maxwell.mitchell@tufts.edu>
2023-11-21 15:10:35 -0500

    last of mta

--- getdata.py
+++ getdata.py
@@ -15,9 +15,41 @@ def jprint(obj):
     text = json.dumps(obj, sort_keys=True, indent=4)
     print(text)
 
-p = {   
-        "$select": "station_complex_id, station_complex, latitude, longitude, borough, routes where transit_timestamp = '2023-10-01T00:00:00.000' and payment_method = 'omny' limit 140000" # should be roughly 7 days worth
-    }
-response = requests.get("https://data.ny.gov/resource/wujg-7c2s.json", params = p)
-# print(response.status_code) # for debugging
-jprint(response.json())
+# p = {   
+#         "$select": "station_complex_id, station_complex, latitude, longitude, borough, routes where transit_timestamp = '2023-10-01T00:00:00.000' and payment_method = 'omny' limit 140000" # should be roughly 7 days worth
+#     }
+# response = requests.get("https://data.ny.gov/resource/wujg-7c2s.json", params = p)
+# # print(response.status_code) # for debugging
+# jprint(response.json())
+
+f = open("station-data.json")
+station_data = json.load(f)
+for outage in station_data:
+    alt_stations = {} 
+    o_routes = outage["routes"].split(",")
+    for station in station_data:
+        if station["station_complex_id"] == outage["station_complex_id"]:
+            continue
+        if not any(r in station["routes"] for r in o_routes):
+            continue
+        p = {
+            "mode":"walking", # TODO could eventually consider allowing the use of bus to get to/from subway
+            "origins":outage["latitude"] + "," + outage["longitude"],
+            "destinations":station["latitude"] + "," + station["longitude"],
+            "key":"AIzaSyCJ3bMTL8NA1UmL3D-ZyWH-0rx98q71vqQ" 
+        }
+        response = requests.get("https://maps.googleapis.com/maps/api/distancematrix/json", params = p)
+        walking_duration = response.json()["rows"][0]["elements"][0]["duration"]["value"] 
+        if int(walking_duration) <= 1800:
+            for r in o_routes:
+                if r in station["routes"]:
+                    if r in alt_stations:
+                        # key already exists, we must compare
+                        if walking_duration < alt_stations[r]["duration"]["value"]:
+                            alt_stations[r] = {"id":station["station_complex_id"], "duration":response.json()["rows"][0]["elements"][0]["duration"]}
+                    else:
+                        alt_stations[r] = {"id":station["station_complex_id"], "duration":response.json()["rows"][0]["elements"][0]["duration"]}
+    jprint(alt_stations)
+
+            # 30 min walk, could be used in place of other station
+            #print(response.json())
--- main.py
+++ main.py
@@ -8,6 +8,7 @@
 ###############################################################
 
 import json 
+import requests
 # TODO:
 # - alternate station generator -> find nearest stations in walking distance from target station
 #       -> evaluate partial vs. true substitutes (true substitute reaches the same stations in similar times) 
@@ -26,15 +27,15 @@ import json
 
 # command control, take user input, station and time range
 f = open("station-data.json")
-stations_codes = json.load(f)
-user_station = ''
+station_data = json.load(f)
 outages = [] # list, extensible to having multiple TODO enable that feature
 
+# ensure station is valid and log it
 def validate_station(s, outages):
-    for station in stations_codes:
+    for station in station_data:
         if s.lower() in station["station_complex"].lower():
             if input("Did you mean '" + station["station_complex"] + "' ? (y/n)    ") == "y":
-                outages.append([station["station_complex"], station["station_complex_id"]])
+                outages.append([station])
                 return True
     print("Could not find '" + s + "' in 'stations-codes.json'.")
     return False
@@ -44,6 +45,7 @@ while (not validate_station(user_station, outages)):
     user_station = input("Please select a valid station:    ")
 
 user_time = ''
+# ensure time is valid and log it
 def validate_time(s, outages):
     days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
     times = s.split("/")
@@ -59,23 +61,44 @@ def validate_time(s, outages):
         timestamps.append("2023-10-0" + str(days.index(data[0].lower()) + 1) + "T" + (data[1]) + ":00:00.000")
     outages[-1].append(timestamps)
     return True
-    
 
 user_time = input("Time format: DAY_OF_WEEK:TIME\nTime range format: START_TIME/END_TIME\nExample: Monday:01/Wednesday:22\nTimes will be rounded down to nearest hour.\nPlease select a time range:    ")
 while (not validate_time(user_time, outages)):
     user_time = input("Please select a time range:    ")
 
+# display user's selection
 print("\n$ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $")
 print("$")
-print("$    Selected outage at: " + outages[-1][0] + ", code: " + outages[-1][1])
-print("$    Internal simulated range " + outages[-1][2][0] + " to " + outages[-1][2][1])
+print("$    Selected outage at: " + outages[-1][0]["station_complex"] + ", code: " + outages[-1][0]["station_complex_id"])
+print("$    Internal simulated range " + outages[-1][1][0] + " to " + outages[-1][1][1])
 print("$")
 print("$ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $ $\n")
+print("\nPlease wait while your alternate stations are calculated...\n")
 
 # alternate station generator
 # TODO find all stations within certain distance of station -> alt stations
-# get location of outage station
-code = outages[-1][1]
+alt_stations = []
+for station in station_data:
+    if station["station_complex_id"] == outages[-1][0]["station_complex_id"]:
+        continue
+    p = {
+        "mode":"walking", # TODO could eventually consider allowing the use of bus to get to/from subway
+        "origins":outages[-1][0]["latitude"] + "," + outages[-1][0]["longitude"],
+        "destinations":station["latitude"] + "," + station["longitude"],
+        "key":"AIzaSyCJ3bMTL8NA1UmL3D-ZyWH-0rx98q71vqQ" # TODO i dont want api key in plaintext in source code... could potentially calculate all distances and determine alternate stations statically and just store the timing
+    } # also, this is super slow...
+    response = requests.get("https://maps.googleapis.com/maps/api/distancematrix/json", params = p)
+    walking_duration = response.json()["rows"][0]["elements"][0]["duration"]["value"] 
+    pretty_wd = response.json()["rows"][0]["elements"][0]["duration"]["text"] 
+    if int(walking_duration) <= 1800:
+        # 30 min walk, could be used in place of other station
+        # TODO give option to modify this distance?
+        #print(response.json())
+        alt_stations.append([station, walking_duration, pretty_wd])
+alt_stations = sorted(alt_stations, key=lambda x: x[1])
+print("Alternate stations to " + outages[-1][0]["station_complex"] + ":")
+for station in alt_stations:
+    print(station[0]["station_complex"] + ", " + station[2])
 # TODO calculate alt station substitutability -> true vs. partial subs
 # TODO distribute riders across range to alt stations -- how do we distribute? 
 
