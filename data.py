735b573a6404c9d256f023612f1d19edb97255f0
Max Mitchell <maxwell.mitchell@tufts.edu>
2023-11-27 16:13:41 -0500

    some work on odds generation

--- data.py
+++ data.py
@@ -10,22 +10,45 @@ import csv
 import pandas as pd
 import requests
 
-df_rail = pd.read_csv('MBTA_Rail_Stop_Cords.csv')
-df_bus = pd.read_csv('MBTA_Bus_Stop_Cords.csv')
+df_rail = pd.read_csv('MBTA_Rail_Ridership_by_Time_Period.csv')
+df_bus = pd.read_csv('MBTA_Bus_Ridership_by_Time_Period.csv')
 
-df_bus = df_bus[['stop_name', 'stop_lat', 'stop_lon', 'Routes']]
-df_rail = df_rail[['stop_name', 'stop_lat', 'stop_lon', 'Routes']]
-df_bus.to_csv('MBTA_Bus_Stops.csv', index=False)
-df_rail.to_csv('MBTA_Rail_Stops.csv', index=False)
+# df_bus = df_bus[['stop_name', 'stop_lat', 'stop_lon', 'Routes']]
+# df_rail = df_rail[['stop_name', 'stop_lat', 'stop_lon', 'Routes']]
+# df_bus.to_csv('MBTA_Bus_Stops.csv', index=False)
+# df_rail.to_csv('MBTA_Rail_Stops.csv', index=False)
 
-# sum_ons_rail = 0
-# sum_offs_rail = 0
-# for index, row in df_rail[['average_ons', 'average_offs']].iterrows():
-#     sum_ons_rail += row['average_ons']
-#     sum_offs_rail += row['average_offs']
+sum_ons_bus = 0
+sums = {
+"very_early_morning_ons" : 0,
+"early_am_ons" : 0,
+"am_peak_ons" : 0,
+"midday_base_ons" : 0,
+"midday_school_ons" : 0,
+"pm_peak_ons" : 0,
+"evening_ons" : 0,
+"late_evening_ons" : 0,
+"night_ons" : 0,
+}
+for index, row in df_rail[['average_ons', 'time_period_name']].iterrows():
+    sums[row['time_period_name'].lower() + '_ons'] += row['average_ons']
 
-# print(sum_ons_rail)
-# print(sum_offs_rail)
+list_odds = []
+for index, row in df_rail[['stop_name', 'average_ons','time_period_name']].iterrows():
+    list_odds.append(row['average_ons'] / sums[row['time_period_name'].lower() + '_ons'])
+    print(row['stop_name'] + ', ' + row['time_period_name'] + ', ' + str(list_odds[-1]))
+
+df_rail[['odds_station_time_on']] = list_odds
+df_rail.to_csv('test.csv',index=False)
+
+# for key in sums:
+#     if 'ons' in key:
+#         print("=============ons================\n" + key + ":\n" + str(sums[key]) + "\n" + str(float(sums[key] / sum_ons_bus)))
+#     else:
+#         print("==============offs===============\n" + key + ":\n" + str(sums[key]) + "\n" + str(float(sums[key] / sum_offs_bus)))
+
+# print(sum_ons_bus)
+# print(sum_offs_bus)
 
 # sum_ons_bus = 0
 # sum_offs_bus = 0
--- genotype.py
+++ genotype.py
@@ -12,41 +12,49 @@ import random
 import pandas as pd
 
 class Time:
-    def __init__(self, text, text_range, start, duration):
+    def __init__(self, text, text_range, start, duration, odds_rail, odds_bus):
         self.text = text
         self.text_range = text_range
         self.start = start
         self.duration = duration # hours
+        self.odds_rail = odds_rail
+        self.odds_bus = odds_bus
 
 times = [
-    Time("VERY_EARLY_MORNING","3am-5:59am",300,3),
-    Time("EARLY_AM","6am-6:59am",600,1),
-    Time("AM_PEAK","7am-8:59am",700,2),
-    Time("MIDDAY_BASE","9am-1:29pm",900,4.5),
-    Time("MIDDAY_SCHOOL","1:30pm-3:59pm",1330,2.5),
-    Time("PM_PEAK","4pm-6:29pm",1600,2.5),
-    Time("EVENING","6:30pm-9:59pm",1830,3.5),
-    Time("LATE_EVENING","10pm-11:59pm",2200,2),
-    Time("NIGHT","12am-2:59am",0,3)
+    # id, text, time, duration, odds_rail, odds_bus (calculated using 2019 data)
+    Time("VERY_EARLY_MORNING","3am-5:59am",300,3,0.013945182241395501,0.06682732111944974),
+    Time("EARLY_AM","6am-6:59am",600,1,0.04850329660828721,0.1551210877786039),
+    Time("AM_PEAK","7am-8:59am",700,2,0.1931098427198099,0.1708102019470824),
+    Time("MIDDAY_BASE","9am-1:29pm",900,4.5,0.18468012539484022,0.11265796173882053),
+    Time("MIDDAY_SCHOOL","1:30pm-3:59pm",1330,2.5,0.13960536103189877,0.19537424182261517),
+    Time("PM_PEAK","4pm-6:29pm",1600,2.5,0.24809902793657512,0.15503673770243018),
+    Time("EVENING","6:30pm-9:59pm",1830,3.5,0.12979677747782262,0.08679233530324226),
+    Time("LATE_EVENING","10pm-11:59pm",2200,2,0.036803057654496374,0.04038162569848716),
+    Time("NIGHT","12am-2:59am",0,3,0.005457328934874285,0.016998486889404713)
 ]
 
-df = pd.read_csv('MBTA_stops.csv')
+df_rail_ridership = pd.read_csv('MBTA_Rail_Ridership_by_Time_Period.csv')
+df_rail_stops = pd.read_csv('MBTA_Rail_Stops.csv')
+df_bus_ridership = pd.read_csv('MBTA_Bus_Ridership_by_Time_Period.csv')
+df_bus_stops = pd.read_csv('MBTA_Bus_Stops.csv')
 
 class Ride:
     # Initialize a random ride
     # modality should be either 'rail' or 'bus'
     def __init__(self, modality):
-        # select random start/end station
-        # TODO weight based on ridership proportions, std deviation
-        
-        # select random start time
-        # TODO weight similarly
+        # select pseudo-random start time, weighted based on ridership proportions
+        selected_time = times[0]
+        for t in times:
+            if random.random() <= (t.odds_rail if modality == 'rail' else t.odds_bus):
+                selected_time = t
+        # select random start/end station, weighted based on ridership proportions
+
         # get route/timing info
         p = {
             "mode":"transit", 
             "transit_mode":modality,
-            "origins":,
-            "destinations":,
+            "origins":o_lat + ', ' + o_lon,
+            "destinations":d_lat + ', ' + d_lon,
             "key":"AIzaSyCJ3bMTL8NA1UmL3D-ZyWH-0rx98q71vqQ" 
         }
         response = requests.get("https://maps.googleapis.com/maps/api/distancematrix/json", params = p)
@@ -54,7 +62,7 @@ class Ride:
         self.dict = {
             "start_point":,
             "end_point":,
-            "start_time":random.choice(times),
+            "start_time":selected_time,
             "end_time":,
             "route":,
         }
